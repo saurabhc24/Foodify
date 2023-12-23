67acfff80c0ae39874c83f1db1b96573d59b41c9
saurabhc24 <saurabhc2001@gmail.com>
2023-12-24 00:57:18 +0530

    learning useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,38 @@
 import RestroCard from "./RestroCard";
-import resList from "../utils/mockData";
 import { useEffect, useState } from "react";
 
 const Body = () => {
-
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
-
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
 
   useEffect(() => {
+    fetchData();
+  },[]);
 
-  }, []);
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.940108244989704&lng=77.73359346961144"
+    );
+
+    const jsonData = await response.json();
+
+    console.log(jsonData);
+
+    setListOfRestaurant(jsonData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
+
   return (
     <div className="body">
       <div className="topRatedBtn">
-        <button className="topRatedResBtn" onClick={() => {
-          const filteredList = listOfRestaurant.filter(
-            (res) => res.info.avgRating > 4
-          );
-          console.log(filteredList);
-          setListOfRestaurant(filteredList);
-        }}>
+        <button
+          className="topRatedResBtn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            );
+            console.log(filteredList);
+            setListOfRestaurant(filteredList);
+          }}
+        >
           <span>Top Rated Restaurants in Bangalore</span>
         </button>
       </div>
