8e1a8420d39dbddbcea9f642d3ba9a48164d7c40
saurabhc24 <saurabhc2001@gmail.com>
2023-12-30 01:26:53 +0530

    trying to add functionality of showing food based on location

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,13 +4,34 @@ import RestroCardShimmer from "./RestroCardShimmer";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [lat, setLat] = useState();
+  const [lng, setLng] = useState();
+
+  // navigator.geolocation.getCurrentPosition((pos) => {
+  //   const { latitude, longitude } = pos.coords;
+  //   setLat(latitude);
+  //   setLng(longitude);
+  // })
+
+  
 
 
   useEffect(() => {
+    function getLocation() {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    function showPosition(position) {
+      const latitude = position.coords.latitude
+      const longitude = position.coords.longitude
+      setLat(latitude);
+      setLng(longitude);
+    }
+    getLocation();
+    console.log("latitude ", lat, "longitude", lng);
     const fetchData = async () => {
       const response = await fetch(
         `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.940108244989704&lng=77.73359346961144`
-        // `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}`
+        // `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${location.}&lng=${lng}`
       );
 
       const jsonData = await response.json();
