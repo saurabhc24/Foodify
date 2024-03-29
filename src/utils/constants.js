fd66ec6d7dbac4f9f1478bb7c16cfa1b0954dad2
saurabhc24 <saurabhc2001@gmail.com>
2024-03-30 02:12:31 +0530

    commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,9 +3,9 @@ export const CDN_IMG_URL =
 
 export const MENU_IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200/";
 
-export const restaurant_fetch_url = "/api/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.940108244989704&lng=77.73359346961144");
+export const restaurant_fetch_url = "https://thingproxy.freeboard.io/fetch/"+encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.940108244989704&lng=77.73359346961144");
 
-export const restaurant_menu_url = "/api/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9395989&lng=77.728955&restaurantId=");
+export const restaurant_menu_url = "https://thingproxy.freeboard.io/fetch/"+encodeURIComponent("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9395989&lng=77.728955&restaurantId=");
 
 
 
