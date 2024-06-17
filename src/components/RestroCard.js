eb1be338ef841967c53fbf55ef056cc4e2eb495e
saurabhc24 <saurabhc2001@gmail.com>
2024-06-17 23:59:41 +0530

    changes in restrocard

--- src/components/RestroCard.js
+++ src/components/RestroCard.js
@@ -1,6 +1,5 @@
 import { CDN_IMG_URL } from "../utils/constants";
 
-
 const RestroCard = (props) => {
   const { restaurant } = props;
 
@@ -10,14 +9,16 @@ const RestroCard = (props) => {
 
   return (
     <div className="p-0 m-0 ">
-      <div className="bg-white shadow-lg hover:cursor-pointer hover:scale-95 mt-7 rounded-lg overflow-hidden w-72">
+      <div className="mt-7 bg-white shadow-lg hover:cursor-pointer hover:scale-95 transition ease-in-out duration-300 rounded-lg overflow-hidden w-72">
         <div
-          className="bg-center bg-cover h-40 w-full relative"
+          className="bg-center bg-cover h-40 w-full relative "
           style={{
             backgroundImage: `url(${CDN_IMG_URL}${restaurant?.info?.cloudinaryImageId})`,
           }}
         >
-          <div className="absolute bg-gray-100 px-2 py-1 text-xs rounded bottom-3 right-3">{deliveryTime} mins</div>
+          <div className="absolute bg-gray-100 px-2 py-1 text-xs rounded bottom-3 right-3">
+            {deliveryTime} mins
+          </div>
           <div className="absolute bg-green-700 px-2 py-1 text-xs text-white rounded bottom-3 left-3">
             {avgRating} &#x2605; | {totalRatingsString}
           </div>
@@ -25,8 +26,12 @@ const RestroCard = (props) => {
         <div className="p-4">
           <div className="flex justify-between items-center">
             <div className="place">
-              <p className="text-xl font-bold text-gray-900 line-clamp-1">{name}</p>
-              <p className="text-sm text-gray-600 line-clamp-1">{cuisines.join(", ")}</p>
+              <p className="text-xl font-bold text-gray-900 line-clamp-1">
+                {name}
+              </p>
+              <p className="text-sm text-gray-600 line-clamp-1">
+                {cuisines.join(", ")}
+              </p>
               <p className="text-sm text-gray-600">{areaName}</p>
             </div>
           </div>
